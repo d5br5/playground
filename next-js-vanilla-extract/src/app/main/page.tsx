4addca6cea027a6086adfb93078a3f880dd64b55
Doh Kim <ehgud456456@naver.com>
2024-11-22 18:12:10 +0900

    remove: useless palette

--- next-js-vanilla-extract/src/app/main/page.tsx
+++ next-js-vanilla-extract/src/app/main/page.tsx
@@ -2,10 +2,16 @@ import * as style from "./styles.css";
 
 const Main = () => {
   return (
-    <div className={style.container}>
-      <h1>Main</h1>
-      <p>Some content</p>
-    </div>
+    <>
+      <div className={style.container}>
+        <h1>Main</h1>
+        <p>Some content</p>
+      </div>
+      <div className={style.box}>
+        <h2>Box</h2>
+        <p>Some content</p>
+      </div>
+    </>
   );
 };
 
--- next-js-vanilla-extract/src/app/main/styles.css.ts
+++ next-js-vanilla-extract/src/app/main/styles.css.ts
@@ -1,7 +1,14 @@
+import { sprinkles } from "@/components/system/styles/sprinkles.css";
 import { themeVars } from "@/components/system/theme/theme.css";
 import { style } from "@vanilla-extract/css";
 
 export const container = style({
   padding: 10,
   color: themeVars.common.thumbnail,
+});
+
+export const box = sprinkles({
+  display: "inline-block",
+  color: "red",
+  zIndex: 1,
 });
